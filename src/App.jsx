7a509dd4cd78fb1b35c1e0261ea0bc6cc41ed893
Oksana Jarosch <ok.jarosch@gmail.com>
2024-03-21 23:05:04 +0100

    icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Suspense } from 'react';
 import { GlobalStyle } from './GlobalStyle';
+import { Route, Routes } from 'react-router-dom';
+import { AppLayout } from './components/AppLayout/AppLayout';
 
 
 function App() {
 
   return (
     <>
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+        </Route>
+
+      </Routes>
+    </Suspense>
 
       <GlobalStyle />
     </>
