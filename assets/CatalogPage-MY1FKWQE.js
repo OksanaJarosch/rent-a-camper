cd56b92e25045257d8abfb058ff4a8b3e337577c
OksanaJarosch <131056473+OksanaJarosch@users.noreply.github.com>
2024-03-23 00:05:21 +0000

    Deploying to gh-pages from @ OksanaJarosch/rent-a-camper@1264355949bc3169fc7afed85ff2d6b1ee3af105 🚀

---
+++ assets/CatalogPage-MY1FKWQE.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-nFOSJcof.js";function o(){return t.jsx("h1",{children:"Catalog"})}export{o as default};
---
+++ assets/HomePage-XdMQ-hhF.js
@@ -0,0 +1,13 @@
+import{u as r,j as e}from"./index-nFOSJcof.js";const i="/rent-a-camper/assets/camper_optimized-B2rFJMRE.webp",a="/rent-a-camper/assets/camper_optimized_retina-D1evYv6S.webp",o=r.div`
+    height: 100vh;
+    background-image: url(${i});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
+    @media (min-device-pixel-ratio: 2),
+        (min-resolution: 192dpi),
+        (min-resolution: 2dppx) {
+            background-image: url(${a});
+        }
+`;function t(){return e.jsx(o,{children:e.jsx("h1",{children:"Wellcome!"})})}export{t as default};
