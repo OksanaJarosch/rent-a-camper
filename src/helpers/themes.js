b5c46190c86d9b42b020132aad239c3b8d300769
Oksana Jarosch <ok.jarosch@gmail.com>
2024-03-24 23:39:18 +0100

    themes

--- src/helpers/themes.js
+++ src/helpers/themes.js
@@ -2,10 +2,19 @@ export const theme = {
     color: {
         orange: "#E44848",
         orangeHover: "#D84343",
+        dark: "#101828",
+        grey: "#475467",
+        yellow: "#FFC531",
+        lightGrey: "#F2F4F7",
+        beige: "#F7F7F7",
     },
     breakpoint: {
         sm: '375px',
         md: '768px',
         xl: '1440px',
     },
+    border: {
+        orange: "1px solid #E44848",
+        grey: "1px solid #475467",
+    }
 }
