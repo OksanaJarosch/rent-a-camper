dc96c49ab58289e0b95e2c1ab95e3a6fef2b771b
OksanaJarosch <131056473+OksanaJarosch@users.noreply.github.com>
2024-04-04 22:16:33 +0000

    Deploying to gh-pages from @ OksanaJarosch/rent-a-camper@28c14e1262907560945913ab80cd7a8e6d9e5126 🚀

--- assets/CatalogPage-DdnZc8Z4.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Dj7O075q.js";function o(){return t.jsx("h1",{children:"Catalog"})}export{o as default};
---
+++ assets/FavoritesPage-yx0OfgM2.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-qPVVeVrg.js";function e(){return r.jsx("h1",{children:"Favorites"})}export{e as default};
