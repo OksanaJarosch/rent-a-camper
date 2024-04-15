1af67f088872a8b32a28ce1d62474a0530ea37dd
OksanaJarosch <131056473+OksanaJarosch@users.noreply.github.com>
2024-04-15 12:47:00 +0000

    Deploying to gh-pages from @ OksanaJarosch/rent-a-camper@30097b7676d30220b90b5ce21ea447b882535c31 🚀

---
+++ assets/FavoritesPage-DsUyNpE4.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CJ_QZ_qB.js";function e(){return r.jsx("h1",{children:"Favorites"})}export{e as default};
--- assets/HomePage-DhYtiXii.js
+++ assets/HomePage-DhYtiXii.js
@@ -1,4 +1,4 @@
-import{u as r,j as e,C as a}from"./index-Bc_QwgBQ.js";const i="/rent-a-camper/assets/camper_optimized-B2rFJMRE.webp",n="/rent-a-camper/assets/camper_optimized_retina-D1evYv6S.webp",o=r.div`
+import{u as r,j as e,C as a}from"./index-CJ_QZ_qB.js";const i="/rent-a-camper/assets/camper_optimized-B2rFJMRE.webp",n="/rent-a-camper/assets/camper_optimized_retina-D1evYv6S.webp",o=r.div`
     height: 100vh;
     background-image: url(${i});
     background-size: cover;
